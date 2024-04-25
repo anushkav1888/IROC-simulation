3716a64fa5b846cece3aecac0938730b3c787bb6
Khush-dev <200010040@iitb.ac.in>
2024-04-25 18:37:40 +0530

    cleanup; idr

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/anushka/iroc_isro/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/anushka/iroc_isro/devel;/opt/ros/noetic'.split(';') if '/home/anushka/iroc_isro/devel;/opt/ros/noetic' != '' else []
--- build/iroc_auto/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anushka/iroc_isro/src/iroc_auto
-# Build directory: /home/anushka/iroc_isro/build/iroc_auto
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
